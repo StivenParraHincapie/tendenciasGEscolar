3c79b14bb52cba519cf6d550363ef5eaa216513e
juanestd <Juanestd1013@hotmail.com>
2024-10-05 22:17:48 -0500

    Modificaciones

--- Gestion_Escuela/app/Courses/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Course
-
-@admin.register(Course)
-class CourseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre_del_curso', 'descripcion', 'profesor', 'horario')
-    search_fields = ('nombre_del_curso',)
